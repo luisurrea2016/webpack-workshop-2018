24e83968fc46a20995432b5eaa187bb6a08f7007
luisurrea2016# <luis.urrea@motortrak.com>
2018-07-08 16:16:13 +0100

    Ad mini css pluong loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import css from './footer.css';
+import btn from  './button.css';
 
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,14 @@ const webpackMerge = require("webpack-merge");
 
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
-module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
-  return webpackMerge(
-    {
-      mode,
-      output: {
-        filename: "bundle.js"
-      },
-      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
-    },
-    modeConfig(mode)
-  );
-};
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) =>
+    webpackMerge(
+        {
+            mode,
+            output: {
+                filename: "bundle.js"
+            },
+            plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+        },
+        modeConfig(mode)
+    );
