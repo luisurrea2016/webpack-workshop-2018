98d34c4d39f695fd5d1505c406d5db2d29d8e5a1
luisurrea2016# <luis.urrea@motortrak.com>
2018-07-15 08:32:55 +0100

    Add Webpack Plugin

---
+++ build-utils/MyFirstWebpackPlugin.js
@@ -0,0 +1,32 @@
+class MyFirstWebpackPlugin {
+    apply(compiler) {
+
+        compiler.hooks.done.tapAsync('MyFirstWebpackPlugin',
+            (stats, cb) => {
+                let assets = [];
+                for (let assetName in stats.compilation.assets) {
+                    assets = [...assets, assetName];
+                }
+                //console.warn(stats);
+                console.warn(assets.join('\n'));
+                //debugger;
+                cb();
+            }
+        );
+
+        compiler.hooks.compilation.tap('MyFirstWebpackPlugin',
+            (compilation, params) => new MyFirstWebpackCompilationPlugin().apply(compilation));
+    }
+}
+
+class MyFirstWebpackCompilationPlugin {
+    apply(compilation) {
+        compilation.hooks.seal.tap('MyFirstWebpackCompilationPlugin',
+            () => {
+                console.warn(compilation);
+                debugger;
+            });
+    }
+}
+
+module.exports = MyFirstWebpackPlugin;
